36d217095af20bb0fefccade9c51dcf8a3e53eb9
tal shachar <tal.shachar@buzzfeed.com>
2017-12-29 05:02:17 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,15 +2,10 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  block_return_values = []
   while i < collection.length
-    block_return_values << yield(collection[i])
+    if yield(collection[i])
+      return collection[i]
+    end
     i = i + 1
-  end
-
-  if block_return_values.include?(false)
-    false
-  else
-    true
   end
 end
