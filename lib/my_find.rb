ed489bb7d66106488796515ad98b3737465ec0de
tal shachar <tal.shachar@buzzfeed.com>
2017-12-29 05:02:43 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,16 +1,11 @@
 require 'pry'
 
-def my_all?(collection)
+def my_find(collection)
   i = 0
-  block_return_values = []
   while i < collection.length
-    block_return_values << yield(collection[i])
+    if yield(collection[i])
+      return collection[i]
+    end
     i = i + 1
-  end
-
-  if block_return_values.include?(false)
-    false
-  else
-    true
   end
 end
